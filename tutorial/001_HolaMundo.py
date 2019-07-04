9c99e6431b1e5a29363aae5c74646997f92e33a4
Cristina Serrano <cristinaserrano@Cristinas-MacBook-Air.local>
2019-07-04 18:38:17 +0200

    Ejercicio 1

--- tutorial/001_HolaMundo.py
+++ tutorial/001_HolaMundo.py
@@ -1,3 +1,4 @@
-# print ("Hola, Mundo")
+print ("Hola, Mundo")
 
 # Ejercicio: pintar un mensaje que diga "Un saludo.Cris" y que incluya un salto de linea delante de Cris. (1 linea)
+print ("Un saludo.\nCris")
