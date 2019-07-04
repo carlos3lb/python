0b3103fb1cdb04407823acdc2e6b4ca3959db519
Cristina Serrano <cristinaserrano@Cristinas-MacBook-Air.local>
2019-07-04 19:11:22 +0200

    Ejercicio 2

--- tutorial/002_Variables&Tipos.py
+++ tutorial/002_Variables&Tipos.py
@@ -4,39 +4,46 @@
 # No es necesario declarar una variable antes de usarla o indicar su tipo
 
 # Números (integer y float)
-# myint = 7
-# print(myint)
+myint = 7
+print(myint)
 
-# myfloat = 7.0
-# print(myfloat)
-# myfloat = float(7)
-# print(myfloat)
+myfloat = 7.0
+print(myfloat)
+
+print(myfloat)
+print(myint+float("7.2"))
 
 # Textos (strings)
 
-# mystring = 'hello'
-# print(mystring)
-# mystring = "hello"
-# print(mystring)
+mystring = 'hello'
+print(mystring)
+mystring = "hello"
+print(mystring)
 
 # Ejercicio: pintar un mensaje utilizando apostrofes (ingles)
+print("I'm pretty")
 
 # Operador +
-# one = 1
-# two = 2
-# three = one + two
-# print(three)
+one = 1
+two = 2
+three = one + two
+print(three)
 
-# hello = "hello"
-# world = "world"
-# helloworld = hello + " " + world
-# print(helloworld)
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
 
 # Ejericio: pintar el mensaje hello world con dos variables utilizando el separador de la funcion print.
+print(hello, world, " ")
 # Ejercicio: funcionaría sumar variables con números y textos? y sin variables (1 + "texto")?
+# print(hello + one)
+# print("Cris" + 5)
 
 # Declarar variables de forma simultanea
-# a, b = 3, 4
-# print(a,b)
+a, b = 3, 4
+print(a,b)
 
 # Ejercicio: funcionaría con tipos distintos?
+a, b = 3, "casa"
+print(a, b)
